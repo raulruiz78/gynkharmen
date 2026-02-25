2b26135a034f1c169a07bc21642984df9209debd
raulruiz78 <96109917+raulruiz78@users.noreply.github.com>
2026-02-25 12:12:06 +0100

    Update answers in clues array to numeric values

--- app/pista/data.js
+++ app/pista/data.js
@@ -1,47 +1,47 @@
 export const clues = [
   {
     id: 1,
-    answer: 'gym donde entrenamos',
+    answer: '1',
     clue: 'Si ese culo quieres duro y bien colocado,\naquí se suda fuerte para dejarlo bien trabajado.'
   },
   {
     id: 2,
-    answer: 'donde raul se compra sus meriendas ibericas',
+    answer: '2',
     clue: 'Cuando el hambre aprieta y no es cualquier antojo,\naquí cae el bocadillo de jamón del bueno, rojo y sabroso.'
   },
   {
     id: 3,
-    answer: 'donde se hizo la adquisicion de la cuchara de oro',
+    answer: '3',
     clue: 'No fue joya ni anillo valioso,\npero aquí nació la famosa cuchara de oro.'
   },
   {
     id: 4,
-    answer: 'zara',
+    answer: '4',
     clue: 'Si entrar te hace feliz sin explicación,\neste es tu parque favorito… tentación tras tentación.'
   },
   {
     id: 5,
-    answer: 'nuestra primera cena romantica',
+    answer: '5',
     clue: 'Entre makis, nigiris y miradas sin prisa,\naquí fue nuestra primera cena de sushi… y empezó la sonrisa.'
   },
   {
     id: 6,
-    answer: 'su cafeteria favorita del mundo',
+    answer: '6',
     clue: 'Si café y calma quieres encontrar,\neste es tu rincón favorito para el mundo pausar.'
   },
   {
     id: 7,
-    answer: 'dar de comer a las ardillas',
+    answer: '7',
     clue: 'Entre árboles, colas y carreras sencillas,\naquí repartimos comida a las ardillas.'
   },
   {
     id: 8,
-    answer: 'estacion de tren para ver al churri',
+    answer: '8',
     clue: 'Abrazos, nervios y beso en el andén,\naquí se espera el tren para ver al churri también.'
   },
   {
     id: 9,
-    answer: 'casa de su angel de la guarda rubio',
+    answer: '9',
     clue: 'Si has llegado hasta aquí sin perder el rumbo,\nesta es la casa de tu ángel rubio… y el final del mundo.'
   }
 ];
