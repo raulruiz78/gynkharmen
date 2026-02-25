b646b22e6d8be88766edaa0fc9109b1d6206cdd6
raulruiz78 <96109917+raulruiz78@users.noreply.github.com>
2026-02-25 10:46:34 +0100

    Crear app móvil de gymkhana con Next.js

---
+++ app/pista/data.js
@@ -0,0 +1,17 @@
+export const clues = [
+  {
+    id: 1,
+    clue: 'Tengo agujas pero no coso. ¿Qué soy?',
+    answer: 'reloj'
+  },
+  {
+    id: 2,
+    clue: 'Cuanto más quitas, más grande me hago. ¿Qué soy?',
+    answer: 'agujero'
+  },
+  {
+    id: 3,
+    clue: 'Tiene llaves pero no abre puertas. ¿Qué es?',
+    answer: 'piano'
+  }
+];
