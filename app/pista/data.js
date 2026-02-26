9f116f28120553d700a83821b2986ee7b0a3d66a
raulruiz78 <96109917+raulruiz78@users.noreply.github.com>
2026-02-26 21:03:33 +0100

    Update answers and clues in data.js

--- app/pista/data.js
+++ app/pista/data.js
@@ -1,47 +1,52 @@
 export const clues = [
   {
     id: 1,
-    answer: '1',
+    answer: '902222224',
     clue: 'Si ese culo quieres duro y bien colocado,\naquí se suda fuerte para dejarlo bien trabajado.'
   },
   {
     id: 2,
-    answer: '2',
+    answer: '390',
     clue: 'Cuando el hambre aprieta y no es cualquier antojo,\naquí cae el bocadillo de jamón del bueno, rojo y sabroso.'
   },
   {
     id: 3,
-    answer: '3',
+    answer: '2016',
     clue: 'No fue joya ni anillo valioso,\npero aquí nació la famosa cuchara de oro.'
   },
   {
     id: 4,
-    answer: '4',
-    clue: 'Si entrar te hace feliz sin explicación,\neste es tu parque favorito… tentación tras tentación.'
+    answer: '2026',
+    clue: 'Restaurante donde no se equivoco en la fecha de reserva'
   },
   {
     id: 5,
-    answer: '5',
-    clue: 'Entre makis, nigiris y miradas sin prisa,\naquí fue nuestra primera cena de sushi… y empezó la sonrisa.'
+    answer: '8',
+    clue: 'Si entrar te hace feliz sin explicación,\neste es tu parque favorito… tentación tras tentación.'
   },
   {
     id: 6,
-    answer: '6',
-    clue: 'Si café y calma quieres encontrar,\neste es tu rincón favorito para el mundo pausar.'
+    answer: '2016',
+    clue: 'Entre makis, nigiris y miradas sin prisa,\naquí fue nuestra primera cena de sushi… y empezó la sonrisa.'
   },
   {
     id: 7,
-    answer: '7',
-    clue: 'Entre árboles, colas y carreras sencillas,\naquí repartimos comida a las ardillas.'
+    answer: '360',
+    clue: 'Si café y calma quieres encontrar,\neste es tu rincón favorito para el mundo pausar.'
   },
   {
     id: 8,
-    answer: '8',
-    clue: 'Abrazos, nervios y beso en el andén,\naquí se espera el tren para ver al churri también.'
+    answer: '1997',
+    clue: 'Entre árboles, colas y carreras sencillas,\naquí repartimos comida a las ardillas.'
   },
   {
     id: 9,
-    answer: '9',
+    answer: '21582114180',
+    clue: 'Abrazos, nervios y beso en el andén,\naquí se espera el tren para ver al churri también.'
+  },
+  {
+    id: 10,
+    answer: 'Carla',
     clue: 'Si has llegado hasta aquí sin perder el rumbo,\nesta es la casa de tu ángel rubio… y el final del mundo.'
   }
 ];
